55977c630c7764881c702f31609d08c812adb3d2
Mel <margherita.g@jumpgroup.it>
2025-04-21 18:48:43 +0200

    Remove links empty

--- src/components/LenisLoader/LenisLoader.client.ts
+++ src/components/LenisLoader/LenisLoader.client.ts
@@ -1,16 +1,49 @@
 import Lenis from 'lenis'
 
+// Extend the Window interface to include the 'lenis' property
+declare global {
+  interface Window {
+    lenis: Lenis;
+  }
+}
+
 export function setupClientLenisLoader() {
 
-  // Initialize Lenis
-  const lenis = new Lenis({
-    autoRaf: true,
-  });
+  // // Initialize Lenis
+  // const lenis = new Lenis({
+  //   autoRaf: true,
+  // });
 
-  // Listen for the scroll event and log the event data
-  lenis.on('scroll', (e) => {
+  // // Listen for the scroll event and log the event data
+  // lenis.on('scroll', (e) => {
 
-  });
+  // });
+
+  // const lenis = new Lenis({
+  //   duration: 10,
+  //   easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)), // https://easings.net
+  //   // direction: 'vertical',
+  //   // smooth: true,
+  //   // smoothTouch: false,
+  //   touchMultiplier: 2,
+  // })
+
+  // window.lenis = lenis;
+
+  // function raf() {
+  //   lenis.raf(performance.now());
+  //   requestAnimationFrame(raf);
+  // }
+
+  // requestAnimationFrame(raf);
+
+  // document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  //   anchor.addEventListener('click', function (this: HTMLAnchorElement, e) {
+  //     e.preventDefault();
+  //     lenis.scrollTo(anchor.getAttribute('href'));
+  //   });
+  // })
+
 }
 
 setupClientLenisLoader();
--- src/components/Stickers/Stickers.client.ts
+++ src/components/Stickers/Stickers.client.ts
@@ -13,57 +13,59 @@ export function setupClientStickers() {
   const loader = document.querySelector(".Stickers__Loader") as HTMLElement;
   const progressBar = document.querySelector(".Stickers__Loadingbar") as HTMLElement;
 
-  // set html overflow to hidden
-  document.documentElement.style.overflow = "hidden";
-
-  function countPercent() {
-    const newPercent = (loadingTl.progress() * 100).toFixed();
-    count.innerHTML = newPercent + "%";
-  }
-
-  const loadingTl = gsap.timeline({
-    paused: false,
-    onUpdate: countPercent,
-    onComplete: loadComplete,
-  });
-
-  loadingTl.to(progressBar, { width: "100%", duration: 2, ease: "power2.inOut" });
-
-  function loadComplete() {
-    count.innerHTML = "100%";
-    progressBar.style.width = "100%";
+  document.addEventListener("DOMContentLoaded", () => {
+    // set html overflow to hidden
+    document.documentElement.style.overflow = "hidden";
   
-      gsap.timeline()
-      .to([loader, count, title], {
-        opacity: 0,
-        duration: 0.3,
-        ease: 'sine.out',
-      })
-      .to(stickers, {
-        y: wrapperHeight ? wrapperHeight * 1.5 : 0,
-        duration: 1.2,
-        ease: "power2.inOut",
-        stagger: {
-          amount: 0.3,
-          from: 'end',
-        },
-      }, '-=0.6')
-      .to(bg, {
-        backdropFilter: 'blur(0px)',
-        duration: 0.2,
-        opacity: 0,
-        ease: 'sine.out',
-      }, '-=0.5')
-      .to(stickersEl, {
-        top: '-10000px',
-        duration: 0.2,
-        zIndex: -20,
-      }, '-=0.4')
-      .to(document.documentElement, {
-        overflow: "auto",
-        duration: 0.1,
-      }, '-=0.4')
-  }
+    function countPercent() {
+      const newPercent = (loadingTl.progress() * 100).toFixed();
+      count.innerHTML = newPercent + "%";
+    }
+  
+    const loadingTl = gsap.timeline({
+      paused: false,
+      onUpdate: countPercent,
+      onComplete: loadComplete,
+    });
+  
+    loadingTl.to(progressBar, { width: "100%", duration: 2, ease: "power2.inOut" });
+  
+    function loadComplete() {
+      count.innerHTML = "100%";
+      progressBar.style.width = "100%";
+    
+        gsap.timeline()
+        .to([loader, count, title], {
+          opacity: 0,
+          duration: 0.3,
+          ease: 'sine.out',
+        })
+        .to(stickers, {
+          y: wrapperHeight ? wrapperHeight * 1.5 : 0,
+          duration: 1.2,
+          ease: "power2.inOut",
+          stagger: {
+            amount: 0.3,
+            from: 'end',
+          },
+        }, '-=0.6')
+        .to(bg, {
+          backdropFilter: 'blur(0px)',
+          duration: 0.2,
+          opacity: 0,
+          ease: 'sine.out',
+        }, '-=0.5')
+        .to(stickersEl, {
+          top: '-10000px',
+          duration: 0.2,
+          zIndex: -20,
+        }, '-=0.4')
+        .to(document.documentElement, {
+          overflow: "auto",
+          duration: 0.1,
+        }, '-=0.4')
+    }
+  });
   
 }
 
